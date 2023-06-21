e6fb6ef2fd4ba8a48b9f73e5783d1071a48f95dd
BayRyan97 <135568984+BayRyan97@users.noreply.github.com>
2023-06-21 19:21:27 -0400

    Update togglePopup.js

--- togglePopup.js
+++ togglePopup.js
@@ -6,3 +6,11 @@ function togglePopup(id) {
         popup.style.display = "none";
     }
 }
+
+let popup = document.getElementById('feature-popup');
+popup.addEventListener('click', function(e) {
+    // If the target of the click isn't the content nor a descendant of the content
+    if (!e.target.closest('.popup-content')) {
+        popup.style.display = "none";
+    }
+});
