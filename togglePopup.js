dd31022049b12cb12c27f1453adf0e970c254648
BayRyan97 <135568984+BayRyan97@users.noreply.github.com>
2023-06-21 19:30:50 -0400

    Update togglePopup.js

--- togglePopup.js
+++ togglePopup.js
@@ -1,24 +1,26 @@
-let featureText = [
-  "Create and sell NFTs of your music with the goal of building something big with your fans and not encouraging a big flip in the secondary market.",
-  "Help establish copyright of your music and have ownership over content.",
-  "Build a relationship with your community like never before.",
-  "Gain insights into your sales with detailed analytics to see how your music is performing in the market."
-];
+document.addEventListener('DOMContentLoaded', (event) => {
+    let featureText = [
+        "Create and sell NFTs of your music with the goal of building something big with your fans and not encouraging a big flip in the secondary market.",
+        "Help establish copyright of your music and have ownership over content.",
+        "Build a relationship with your community like never before.",
+        "Gain insights into your sales with detailed analytics to see how your music is performing in the market."
+    ];
 
-document.querySelectorAll('.feature-item').forEach((item, i) => {
-  item.addEventListener('click', function() {
+    document.querySelectorAll('.feature-item').forEach((item, i) => {
+        item.addEventListener('click', function() {
+            let popup = document.getElementById('feature-popup');
+            let popupText = popup.querySelector('.popup-text');
+            let popupHeader = popup.querySelector('.popup-header');
+            popupText.textContent = featureText[i];
+            popupHeader.textContent = this.querySelector('p').textContent;
+            popup.style.display = 'block';
+        });
+    });
+
     let popup = document.getElementById('feature-popup');
-    let popupText = popup.querySelector('.popup-text');
-    let popupHeader = popup.querySelector('.popup-header');
-    popupText.textContent = featureText[i];
-    popupHeader.textContent = this.querySelector('p').textContent;
-    popup.style.display = 'block';
-  });
-});
-
-let popup = document.getElementById('feature-popup');
-popup.addEventListener('click', function(e) {
-  if (e.target == popup) {
-    popup.style.display = 'none';
-  }
+    popup.addEventListener('click', function(e) {
+        if (e.target == popup) {
+            popup.style.display = 'none';
+        }
+    });
 });
