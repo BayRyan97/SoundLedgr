6450daf9b1cc5bdd648e2b3860bfed1784959136
BayRyan97 <135568984+BayRyan97@users.noreply.github.com>
2023-06-21 19:39:09 -0400

    Delete togglePopup.js

--- togglePopup.js
+++
@@ -1,8 +0,0 @@
-function togglePopup(id) {
-    var popup = document.getElementById(id);
-    if (popup.style.display === "none" || popup.style.display === "") {
-        popup.style.display = "block";
-    } else {
-        popup.style.display = "none";
-    }
-}
